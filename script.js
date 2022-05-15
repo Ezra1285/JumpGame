753a3e0d6406788f9351cb3cc291317ecb0d3508
ezra <ezraskoog@yahoo.com>
2022-05-15 16:26:30 -0600

    Added a high score counter to keep track of personal high scores.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 document.addEventListener("click", jump);
 var isGamePlaying = 0; // 0 for when the game if off and 1 when the game is on.
+var totalScore = 0;
+var highScoreList = [];
 
 var startButton = document.getElementById("startButton");
 startButton.onclick = function () {
@@ -20,6 +22,9 @@ function checkDead() {
     console.log("Death");
     isGamePlaying = 0;
     block.classList.remove("startGame");
+    console.log(totalScore);
+    highScoreList.push(totalScore);
+    totalScore = 0;
     alert("Game over");
   }
 }
@@ -45,4 +50,68 @@ function jump() {
 }
 function removeJump() {
   character.classList.remove("animate");
+}
+
+/*
+Counting the players score.
+*/
+
+//  Get blocks left position and check if it is passed
+function countScore() {
+  let isScoreCountedAlready = false;
+  let blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  let characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+
+  if (
+    blockLeft < 20 &&
+    blockLeft > -20 &&
+    characterTop < 130 &&
+    isScoreCountedAlready == false
+  ) {
+    console.log("Score added");
+    totalScore++;
+    isScoreCountedAlready = true;
+  }
+}
+
+setInterval(countScore, 50);
+
+/*
+Creating the leaderboard
+*/
+
+// document.addEventListener("click", () => {
+function updateScoreBoard() {
+  let scoreList = [];
+  let tableElements = document.querySelector("#leaderBoard");
+  let scoreElements = document.getElementsByClassName(".score");
+
+  //  Add each element(score) to the array
+  tableElements.querySelectorAll(".score").forEach((el) => scoreList.push(el));
+
+  //  Clear our table or the previous scores
+  for (let i = 0; i < scoreList.length; i++) {
+    console.log("Beep" + scoreList[i].innerHTML);
+  }
+  //  Sort the array for to reinsert it into the table
+  let sortedScoreList = scoreList.sort(function (a, b) {
+    console.log("Testing:" + parseInt(a, 10) + parseInt(b, 10));
+    return a - b;
+  });
+
+  for (let i = 0; i < sortedScoreList.length; i++) {
+    console.log(sortedScoreList[i]);
+    console.log("Test");
+  }
+  //  Re-add elements to the table
+  // sortedScoreList.forEach((e) => tableElements.appendChild(e));
+  let myTable = document.getElementById("leaderBoard").rows;
+  for (let i = 1; i < 4; i++) {
+    let y = myTable[i].cells;
+    y[1].innerHTML = highScoreList[i];
+  }
 }
